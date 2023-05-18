46e98037bb90d4513b4da5d25aca8d06a41050c8
nasenblick <info@nasenblick.de>
2023-05-18 09:09:19 +0200

    Incorporated mentoring feedback to ocr_numbers

--- ocr-numbers/ocr_numbers.py
+++ ocr-numbers/ocr_numbers.py
@@ -11,81 +11,65 @@ NUMBERS = [
     ' _ |_| _|   '
     ]
 
-def convert(input_grid: list[str]) -> str:
-    if len(input_grid) % 4 != 0:
-        raise ValueError("Number of input lines is not a multiple of four")
-    
-    if any(len(s) % 3 != 0 for s in input_grid):
+def convert(input_grid: list[str]) -> str: 
+    if len(input_grid) % 4 != 0: 
+        raise ValueError("Number of input lines is not a multiple of four") 
+    if any(len(s) % 3 != 0 for s in input_grid): 
         raise ValueError("Number of input columns is not a multiple of three")
-
+    
     resized = False
-
+    
     if len(input_grid) > 4:
         resized = True
         input_grid = resize(input_grid)
 
-    columns = []
-    rows = []
-    col_count = max(len(c) for c in input_grid)
-
-    i = 0    
-    while i < col_count:
-            columns = [s[i:i+3] for s in input_grid]
-            rows.append(''.join(columns))
-            i += 3
+    rows = [''.join([input_grid[i][j:j+3] for i in range(len(input_grid))]) for j in range(0, len(input_grid[0]), 3)]
     return format(rows, resized)
 
-
-def format(rows: list, resized: bool) -> str:
-    result = []
-    for number in rows:
+def format(rows: list, resized: bool) -> str: 
+    result = [] 
+    for number in rows: 
         if number not in NUMBERS: 
-            result += '?'
-        for idx, item in enumerate(NUMBERS):
-            if number == item: 
-                result += str(idx)
-                if idx % 3 == 0 and resized:
-                    result += ','
+            result.append('?') 
+        else: result.append(str(NUMBERS.index(number))) 
+            
+        if NUMBERS.index(number) % 3 == 0 and resized: 
+            result.append(',') 
     return ''.join(result).rstrip(',') if result else '?'
 
-
-def resize(input_grid: list[str]) -> list[str]:
-    resized_grid = []
-    for idx, row in enumerate(input_grid):
-        if 0 <= idx < 4:
-            resized_grid.append(row)
-        elif 4 <= idx < 8:
-            resized_grid[idx-4] += row
-        elif 8 <= idx < 12:
-            resized_grid[idx-8] += row
-        else:
-            return -1
+def resize(input_grid: list[str]) -> list[str]: 
+    resized_grid = [] 
+    for idx, row in enumerate(input_grid): 
+        if 0 <= idx < 4: 
+            resized_grid.append(row) 
+        else: 
+            resized_grid[idx % 4] += row 
     return resized_grid
-    
+
 #print(convert([" _ ", "| |", "|_|", "   "]))
 
 
-# print(convert(
-#                 [
-#                     "    _  _     _  _  _  _  _  _ ",
-#                     "  | _| _||_||_ |_   ||_||_|| |",
-#                     "  ||_  _|  | _||_|  ||_| _||_|",
-#                     "                              ",
-#                 ]))
-
-print(convert([
-                    "    _  _ ",
-                    "  | _| _|",
-                    "  ||_  _|",
-                    "         ",
-                    "    _  _ ",
-                    "|_||_ |_ ",
-                    "  | _||_|",
-                    "         ",
-                    " _  _  _ ",
-                    "  ||_||_|",
-                    "  ||_| _|",
-                    "         ",
+print(convert(
+                [
+                    "    _  _     _  _  _  _  _  _ ",
+                    "  | _| _||_||_ |_   ||_||_|| |",
+                    "  ||_  _|  | _||_|  ||_| _||_|",
+                    "                              ",
                 ]))
+
+# print(convert([
+#                     "    _  _ ",
+#                     "  | _| _|",
+#                     "  ||_  _|",
+#                     "         ",
+#                     "    _  _ ",
+#                     "|_||_ |_ ",
+#                     "  | _||_|",
+#                     "         ",
+#                     " _  _  _ ",
+#                     "  ||_||_|",
+#                     "  ||_| _|",
+#                     "         ",
+#                 ]))
 
 
