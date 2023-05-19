341c1076d10ed76b7e2a83865c6059d311ca8eaa
nasenblick <info@nasenblick.de>
2023-05-19 17:09:13 +0200

    minesweeper completed

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,25 +1,34 @@
 def annotate(minefield):
-    count = 0
-    rows = len(minefield[0])
+
+    rows = any(len(m) for m in minefield)
     cols = len(minefield)
+    count = 0
+
+    if not rows:
+        return minefield
+    
+    if not all(len(m) == len(minefield[0]) for m in minefield):
+        raise ValueError("The board is invalid with current input.")
+
+    if any(item != ' ' and item != '*' for row in minefield for item in row):
+        raise ValueError("The board is invalid with current input.")
 
     for idc, column in enumerate(minefield):
         for idr, row in enumerate(column):
-            count = 0
             try:
-                if minefield[idc - 1][idr] == '*':
+                if idc > 0 and minefield[idc - 1][idr] == '*':
                     count += 1
             except IndexError:
                 pass
 
             try:
-                if minefield[idc - 1][idr - 1] == '*':
+                if idc > 0 and idr > 0 and minefield[idc - 1][idr - 1] == '*':
                     count += 1
             except IndexError:
                 pass
 
             try:
-                if minefield[idc - 1][idr + 1] == '*':
+                if idc > 0 and minefield[idc - 1][idr + 1] == '*':
                     count += 1
             except IndexError:
                 pass
@@ -29,6 +38,7 @@ def annotate(minefield):
                     count += 1
             except IndexError:
                 pass
+                
             try:
                 if minefield[idc + 1][idr + 1] == '*':
                     count += 1
@@ -36,73 +46,28 @@ def annotate(minefield):
                 pass
         
             try:
-                if minefield[idc][idr - 1] == '*':
+                if idr > 0 and minefield[idc][idr - 1] == '*':
                     count += 1
             except IndexError:
                 pass
+                
             try:
                 if minefield[idc][idr + 1] == '*':
                     count += 1
             except IndexError: 
                 pass
-            if minefield[idc][idr] != '*':
+                
+            try:
+                if idr > 0 and minefield[idc + 1][idr - 1] == '*':
+                    count += 1
+            except IndexError: 
+                pass
+                
+            if minefield[idc][idr] != '*' and count > 0:
                 colTmp = list(minefield[idc])
                 colTmp[idr] = str(count)
                 minefield[idc] = "".join(colTmp)
-    
-    # #Ecke OL
-    # if minefield[0][1] == '*':
-    #     count += 1
-    # if minefield[1][1] == '*':
-    #     count += 1
-    # if minefield[1][0] == '*':
-    #     count += 1
-    # if minefield[0][0] != '*':
-    #     col0 = list(minefield[0])
-    #     col0[0] = str(count)
-    #     minefield[0] = "".join(col0)   
-    # count = 0     
-
-    # #Ecke OR
-    # if minefield[cols - 2][0] == '*':
-    #     count += 1
-    # if minefield[cols - 2][1] == '*':
-    #     count += 1
-    # if minefield[cols - 1][1] == '*':
-    #     count += 1
-    # if minefield[cols - 1][0] != '*':
-    #     colY = list(minefield[cols-1])
-    #     colY[0] = str(count)
-    #     minefield[cols-1] = ''.join(colY)
-    # count = 0  
-
-    # #Ecke UL
-    # if minefield[0][rows - 2] == '*':
-    #     count += 1
-    # if minefield[1][rows - 2] == '*':
-    #     count += 1
-    # if minefield[1][rows - 1] == '*':
-    #     count += 1
-    # if minefield[0][rows - 1] != '*':
-    #     row0 = list(minefield[0])
-    #     row0[rows - 1] = str(count)
-    #     minefield[0] = ''.join(row0)
-    # count = 0
-
-
-    # #Ecke UR
-    # if minefield[cols - 2][rows - 1] == '*':
-    #     count += 1
-    # if minefield[cols - 2][rows - 2] == '*':
-    #     count += 1
-    # if minefield[cols - 1][rows - 2] == '*':
-    #     count += 1
-    # if minefield[cols -1][rows - 1] != '*':
-    #     rowX = list(minefield[cols -1])
-    #     rowX[cols - 1] = str(count)
-    #     minefield[cols - 1] = ''.join(rowX)
-    # count = 0
-    
+            count = 0
     return minefield
 
-print(annotate(["   ", " * ", "   "]))
+print(annotate([" ", "*  ", "  "]))
