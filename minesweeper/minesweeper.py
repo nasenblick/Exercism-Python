8722cc8f0baa465470cb3e1c8a82910f4f1a477f
nasenblick <info@nasenblick.de>
2023-05-21 17:28:22 +0200

    Refactoring Minesweeper

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,32 +1,26 @@
 def annotate(minefield: list[str]) -> list[str]:
 
-    if not minefield:
-        return minefield
-
     for row in minefield:
         if len(row) != len(minefield[0]) or any(c not in " *" for c in row):
             raise ValueError('The board is invalid with current input.')
     
-    rows = len(minefield[0])
+    rows = len(minefield[0]) if minefield else 0
     cols = len(minefield)
-    count = 0
 
     for idc, column in enumerate(minefield):
         for idr, row in enumerate(column):
-            POSC = [idc - 1, idc - 1, idc - 1, idc + 1, idc + 1, idc, idc, idc + 1]
-            POSR = [idr, idr - 1, idr + 1, idr, idr + 1, idr - 1, idr + 1, idr - 1]
-            #if 0 < idc < cols - 1 and 0 < idr < rows - 1:
-            print('idc',idc, 'idr', idr)
-            for posc, posr in zip(POSC, POSR):
-                if 0 <= posc < cols and 0 <= posr < rows:
-                    if minefield [posc][posr] == '*':
-                        count += 1
-               
-            if minefield[idc][idr] != '*' and count > 0:
-                colTmp = list(minefield[idc])
-                colTmp[idr] = str(count)
-                minefield[idc] = "".join(colTmp)
             count = 0
+            for posc in (-1, 0, 1):
+                for posr in (-1, 0, 1):
+                    if 0 <= idc + posc < cols and 0 <= idr + posr < rows:
+                        print('idc', idc, 'posc', posc, 'idr', idr, 'posr', posr)
+                        if minefield[idc + posc][idr + posr] == '*':
+                            count += 1
+
+                    if minefield[idc][idr] != '*' and count > 0:
+                        col_tmp = list(minefield[idc])
+                        col_tmp[idr] = str(count)
+                        minefield[idc] = "".join(col_tmp)
     return minefield
 
 print(annotate(["   ", " * ", "   "]))
