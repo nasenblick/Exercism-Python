d209e4c03657d3c587c021b4375948573ebc376a
nasenblick <info@nasenblick.de>
2023-05-21 19:37:56 +0200

    Minesweper iteration #5

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,8 +1,10 @@
 def annotate(minefield: list[str]) -> list[str]:
 
-    for row in minefield:
-        if len(row) != len(minefield[0]) or any(c not in " *" for c in row):
-            raise ValueError('The board is invalid with current input.')
+    if not all(len(m) == len(minefield[0]) for m in minefield):
+        raise ValueError("The board is invalid with current input.")
+    
+    if any(item not in ' *' for row in minefield for item in row):
+        raise ValueError("The board is invalid with current input.")
     
     rows = len(minefield[0]) if minefield else 0
     cols = len(minefield)
@@ -10,17 +12,25 @@ def annotate(minefield: list[str]) -> list[str]:
     for idc, column in enumerate(minefield):
         for idr, row in enumerate(column):
             count = 0
+            
             for posc in (-1, 0, 1):
                 for posr in (-1, 0, 1):
                     if 0 <= idc + posc < cols and 0 <= idr + posr < rows:
-                        print('idc', idc, 'posc', posc, 'idr', idr, 'posr', posr)
                         if minefield[idc + posc][idr + posr] == '*':
                             count += 1
+            col = list(minefield[idc])
+            if minefield[idc][idr] != '*' and count:
+                col[idr] = str(count)     
+            minefield[idc] = ''.join(col) 
 
-                    if minefield[idc][idr] != '*' and count > 0:
-                        col_tmp = list(minefield[idc])
-                        col_tmp[idr] = str(count)
-                        minefield[idc] = "".join(col_tmp)
     return minefield
 
 print(annotate(["   ", " * ", "   "]))
+
+
+            # if minefield[idc][idr] != '*' and count:
+            #     print('idc', idc, 'idr', idr, 'count', count)
+            #     value += str(count)
+            #     print('value', value)
+            #     minefield[idc] = value
+            #     print('minefield', minefield)
