bda9d625023dd9ec679da72edf7a944841d0d81b
nasenblick <info@nasenblick.de>
2023-05-24 13:01:36 +0200

    Wechselt korrekt aber nicht effizient

--- change/change.py
+++ change/change.py
@@ -1,2 +1,10 @@
 def find_fewest_coins(coins, target):
-    pass
+    change = []
+    while target:
+        for coin in coins[::-1]:
+            if target >= coin:
+                change.append(coin)
+                target -= coin
+    return change
+
+print(find_fewest_coins([1, 4, 15, 20, 50], 23))
